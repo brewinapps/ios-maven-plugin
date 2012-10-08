2418f95ecd453c7fc40a2a0559176593c9471ae9
Christian Wack <christian.wack@letsdev.de>
2012-10-09 00:01:45 +0200

    added unlock keychain password

--- src/main/java/de/letsdev/maven/plugins/ios/ProjectBuilder.java
+++ src/main/java/de/letsdev/maven/plugins/ios/ProjectBuilder.java
@@ -133,8 +133,14 @@ public class ProjectBuilder {
 
         //unlock keychain
         if(properties.containsKey("keychainPath") && properties.containsKey("keychainPassword")) {
-            String command = "security unlock-keychain -p \'" + properties.get("keychainPassword") + "\' " + properties.get("keychainPath");
-            processBuilder = new ProcessBuilder(CommandHelper.getCommand(command));
+            processBuilder = new ProcessBuilder(
+                    "security unlock-keychain",
+                    "-p",
+                    properties.get("keychainPassword"),
+                    properties.get("keychainPath")
+            );
+
+            processBuilder = new ProcessBuilder(processBuilder);
             processBuilder.directory(workDir);
             CommandHelper.performCommand(processBuilder);
         }
