28863be69eb7cce92d81c19d7ace4d58dbd5cad9
Arne Fischer <arne.fischer@letsdev.de>
2013-10-22 11:07:39 +0200

    manually add retina icons to plist

--- src/main/resources/META-INF/write-app-icon-name.sh
+++ src/main/resources/META-INF/write-app-icon-name.sh
@@ -11,15 +11,24 @@ APP_ICON_SUFFIX="${NEW_APP_ICON_NAME##*.}"
 /usr/libexec/PlistBuddy -c "Delete :CFBundleIcons" $PLIST_LOCATION
 /usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:CFBundleIconFiles array" $PLIST_LOCATION
 /usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':0 string $APP_ICON_PREFIX-settings-29.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':1 string $APP_ICON_PREFIX-spotlight-40.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':2 string $APP_ICON_PREFIX-iphone-57.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':3 string $APP_ICON_PREFIX-iphone-60.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':1 string $APP_ICON_PREFIX-settings-29@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':2 string $APP_ICON_PREFIX-spotlight-40.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':3 string $APP_ICON_PREFIX-spotlight-40@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':4 string $APP_ICON_PREFIX-iphone-57.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':5 string $APP_ICON_PREFIX-iphone-57@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':6 string $APP_ICON_PREFIX-iphone-60.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons:CFBundlePrimaryIcon:'CFBundleIconFiles':7 string $APP_ICON_PREFIX-iphone-60@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
 
 
 /usr/libexec/PlistBuddy -c "Delete :CFBundleIcons~ipad" $PLIST_LOCATION
 /usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:CFBundleIconFiles array" $PLIST_LOCATION
 /usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':0 string $APP_ICON_PREFIX-settings-29.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':1 string $APP_ICON_PREFIX-spotlight-40.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':2 string $APP_ICON_PREFIX-spotlight-50.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':3 string $APP_ICON_PREFIX-ipad-72.$APP_ICON_SUFFIX" $PLIST_LOCATION
-/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':4 string $APP_ICON_PREFIX-ipad-76.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':1 string $APP_ICON_PREFIX-settings-29@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':2 string $APP_ICON_PREFIX-spotlight-40.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':3 string $APP_ICON_PREFIX-spotlight-40@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':4 string $APP_ICON_PREFIX-spotlight-50.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':5 string $APP_ICON_PREFIX-spotlight-50@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':6 string $APP_ICON_PREFIX-ipad-72.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':7 string $APP_ICON_PREFIX-ipad-72@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':8 string $APP_ICON_PREFIX-ipad-76.$APP_ICON_SUFFIX" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Add :CFBundleIcons~ipad:CFBundlePrimaryIcon:'CFBundleIconFiles':9 string $APP_ICON_PREFIX-ipad-76@2x.$APP_ICON_SUFFIX" $PLIST_LOCATION
