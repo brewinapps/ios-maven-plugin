8cf2400281c5ccb3cd96e076d66b2abedcec2c19
Fabian Köbel <fabian.koebel@letsdev.de>
2016-09-07 14:35:25 +0200

    fixed app store upload.

--- src/main/resources/META-INF/upload-app.sh
+++ src/main/resources/META-INF/upload-app.sh
@@ -8,6 +8,7 @@ PASSWORD=$3
 XCODE_VERSION=$4
 
 export PATH=$PATH:$XCODE_VERSION/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/Current/Support/:$XCODE_VERSION/Contents/Applications/Application\ Loader.app/Contents/itms/:$XCODE_VERSION/Contents/Applications/Application\ Loader.app/Contents/itms/bin
+export PATH=$PATH:$XCODE_VERSION/../Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/Current/Support/:$XCODE_VERSION/../Applications/Application\ Loader.app/Contents/itms/:$XCODE_VERSION/../Applications/Application\ Loader.app/Contents/itms/bin
 
 sudo ln -s -f $XCODE_VERSION/Contents/Applications/Application\ Loader.app/Contents/itms/ /usr/local/itms
 
