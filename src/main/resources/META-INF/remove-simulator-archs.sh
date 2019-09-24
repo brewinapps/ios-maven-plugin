bcf1b0f35f0fb59f9fbf8760a6baf8dc176c3730
Fabian Köbel <fabian.koebel@letsdev.de>
2019-09-24 14:27:12 +0200

    shell script clean up.

--- src/main/resources/META-INF/remove-simulator-archs.sh
+++ src/main/resources/META-INF/remove-simulator-archs.sh
@@ -4,13 +4,6 @@ APP_PATH="$1"
 echo $1" - is the path!!"
 find "$APP_PATH" -name '*.framework' -type d | while read -r FRAMEWORK
 do
-##if [ -e "$FRAMEWORK/Info.plist" ]; then
-#    # do with info from plist
-#    # FRAMEWORK_EXECUTABLE_NAME=$(defaults read "$FRAMEWORK/Info.plist" CFBundleExecutable)
-#    # FRAMEWORK_EXECUTABLE_PATH="$FRAMEWORK/$FRAMEWORK_EXECUTABLE_NAME"
-#    # lipo -remove
-##else
-#    #do brutal
     echo "Trying to remove simulator architectures from framework: ""$FRAMEWORK"
     for subFile in "$FRAMEWORK"/*; do
     if [[ -f "$subFile" ]]; then
