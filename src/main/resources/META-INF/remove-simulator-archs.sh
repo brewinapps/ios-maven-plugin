814cf8698df40fe4ab24f8f3d7eaf1c3f66bbb6c
Fabian Köbel <fabian.koebel@letsdev.de>
2019-09-24 14:46:38 +0200

    shell script clean up.

--- src/main/resources/META-INF/remove-simulator-archs.sh
+++ src/main/resources/META-INF/remove-simulator-archs.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 APP_PATH="$1"
-echo $1" - is the path!!"
 find "$APP_PATH" -name '*.framework' -type d | while read -r FRAMEWORK
 do
     echo "Trying to remove simulator architectures from framework: ""$FRAMEWORK"
     for subFile in "$FRAMEWORK"/*; do
     if [[ -f "$subFile" ]]; then
     if [[ -x "$subFile" ]]; then
-        lipo -remove i386 "$subFile" -o "$subFile" >> 2&>1 /dev/null;
-        lipo -remove x86_64 "$subFile" -o "$subFile" >> 2&>1 /dev/null;
+        echo "Trying to remove i386 from $subFile"
+        lipo -remove i386 "$subFile" -o "$subFile";
+        echo "Trying to remove x86_64 from $subFile"
+        lipo -remove x86_64 "$subFile" -o "$subFile";
     fi
     fi
     done
