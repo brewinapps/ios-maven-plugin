7e53736c2ef717b50c70f59747afba9d00f18ca8
Fabian Köbel <fabian.koebel@letsdev.de>
2019-10-31 10:46:56 +0100

    code cleanup.

--- src/main/java/de/letsdev/maven/plugins/ios/mojo/BaseMojo.java
+++ src/main/java/de/letsdev/maven/plugins/ios/mojo/BaseMojo.java
@@ -12,25 +12,19 @@
 package de.letsdev.maven.plugins.ios.mojo;
 
 import de.letsdev.maven.plugins.ios.CommandHelper;
-import de.letsdev.maven.plugins.ios.ProjectBuilder;
 import de.letsdev.maven.plugins.ios.ProvisioningProfileData;
 import de.letsdev.maven.plugins.ios.ProvisioningProfileHelper;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import de.letsdev.maven.plugins.ios.mojo.container.FileReplacement;
 import de.letsdev.maven.plugins.ios.Utils;
@@ -434,12 +428,14 @@ public class BaseMojo extends AbstractMojo {
 
     /**
      * the username for iTunesConnect
+     *
      * @parameter iTunesConnectUsername
      */
     private String iTunesConnectUsername;
 
     /**
      * the password for iTunesConnect
+     *
      * @parameter iTunesConnectPassword
      */
     private String iTunesConnectPassword;
