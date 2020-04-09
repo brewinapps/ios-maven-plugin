ff4222517d6c5154881206122f17c902afd12a95
Fabian Köbel <fabian.koebel@letsdev.de>
2020-04-09 16:17:06 +0200

    fixed reset of simulators.

--- src/main/resources/META-INF/reset-simulators.sh
+++ src/main/resources/META-INF/reset-simulators.sh
@@ -4,6 +4,7 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
 osascript -e 'tell application "iOS Simulator" to quit'
 osascript -e 'tell application "Simulator" to quit'
-xcrun simctl shutdown all
 killall "Simulator" 2> /dev/null
+xcrun simctl shutdown all
+xcrun simctl shutdown all
 xcrun simctl erase all
