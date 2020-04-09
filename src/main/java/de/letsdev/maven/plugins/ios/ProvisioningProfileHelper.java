db88ba11bc94ac1cfcbc125ec32d7ea961904c02
Mika Auer <mika.auer@letsdev.de>
2020-04-09 11:25:22 +0200

    added copyright

--- src/main/java/de/letsdev/maven/plugins/ios/ProvisioningProfileHelper.java
+++ src/main/java/de/letsdev/maven/plugins/ios/ProvisioningProfileHelper.java
@@ -1,3 +1,14 @@
+/**
+ * ios-maven-plugin
+ * <p/>
+ * User: mauer
+ * Date: 2019-07-09
+ * <p/>
+ * This code is copyright (c) 2019 let's dev.
+ * URL: https://www.letsdev.de
+ * e-Mail: contact@letsdev.de
+ */
+
 package de.letsdev.maven.plugins.ios;
 
 import de.letsdev.maven.plugins.ios.mojo.BaseMojo;
