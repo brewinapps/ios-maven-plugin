c617139db3532a894c233d336c41e221b567ca3e
Fabian Köbel <fabian.koebel@letsdev.de>
2020-10-29 10:42:20 +0100

    shutdown all booted simulators.

--- src/main/resources/META-INF/reset-simulators.sh
+++ src/main/resources/META-INF/reset-simulators.sh
@@ -5,6 +5,6 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 osascript -e 'tell application "iOS Simulator" to quit'
 osascript -e 'tell application "Simulator" to quit'
 killall "Simulator" 2> /dev/null
-xcrun simctl shutdown all
-xcrun simctl list | grep Booted | awk -F "[()]" '{ for (i=2; i<NF; i+=2) print $i }' | grep '^[-A-Z0-9]*$' | xargs -I uuid xcrun simctl shutdown uuid
-xcrun simctl erase all
+xcrun simctl shutdown all 2> /dev/null
+xcrun simctl list | grep Booted | awk -F "[()]" '{ for (i=2; i<NF; i+=2) print $i }' | grep '^[-A-Z0-9]*$' | xargs -I uuid xcrun simctl shutdown uuid 2> /dev/null
+xcrun simctl erase all 2> /dev/null
